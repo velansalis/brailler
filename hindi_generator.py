fc95970c68024ecf8d222240faf96de5f9face11
velansalis <velanmax8@gmail.com>
2019-03-21 19:28:45 +0530

    Done with json

--- hindi_generator.py
+++ hindi_generator.py
@@ -1,8 +1,8 @@
-f = open("hindi_unicode.txt", "w")
+f = open("hindi_unicode_beta.txt", "w")
 
 for i in range(2304, 2304+127):
-    print(i, " - ", chr(i))
-    f.write(str(i) + " - ")
+    print(chr(i))
+    #f.write(str(i) + " - ")
     f.write(chr(i))
     f.write("\n")
 
