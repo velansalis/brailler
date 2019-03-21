1c84ed9242ba2c681768ec0610a5908d0a3e4fd9
velansalis <velanmax8@gmail.com>
2019-03-22 03:18:06 +0530

    Braille conversion: Done

--- hindi_words.py
+++ hindi_words.py
@@ -1,31 +1,43 @@
-# Built in JSON package to convert the data
 import json
+from collections import OrderedDict
 
-# Loading the JSON Data of the Mappings to a dictionary
-with open("hindi-to-braille.json") as j:
-    data = json.load(j)[0]
+with open("hindi-braille-mapping.json") as j:
+    data = json.load(j, object_pairs_hook=OrderedDict)[0]
 
 
 def convert_to_braille(word, write=False):
     if write:
-        f = open("braille-text.txt", "a")
+        f = open("hindi-to-braille-text.txt", "a")
         f.write("Hindi Sentence : " + word + "\n")
         f.write("Braille Sentence : ")
-    # Printing the actual word
-    print("\n" + word + "-", end=" ")
-    # Loop over the word and find the braille equivalent
     for i in list(word):
         if write:
             f.write(data[i][0])
-        print(data[i][0], end=" ")
     f.write("\n")
     f.flush()
     f.close()
-    # One line to end this up
     print()
 
 
-# Word that needs to be converted.
-# Pass true as second parameter if you want the output in a file
-convert_to_braille(
-    "अनुच्छेद १ सभी मनुष्यों को गौरव और अधिकारों के मामले में जन्मजात स्वतन्त्रता और समानता प्राप्त हैं", True)
+def convert_to_hindi(word, write=False):
+    string = ""
+    if write:
+        f = open("braille-to-hindi-text.txt", "a")
+        f.write("Braille Sentence : " + word + "\n")
+        f.write("Hindi Sentence : ")
+    for i in word:
+        wordlist = []
+        for hindi, braille in data.items():
+            if not isinstance(braille, str) and braille[0] == i:
+                wordlist.append(hindi)
+        if len(string) != 0 and len(wordlist) > 1:
+            string += wordlist[1]
+        else:
+            string += wordlist[0]
+    if write:
+        f.write(string + "\n")
+        f.flush()
+        f.close()
+
+#convert_to_braille("अनुच्छेद सभी मनुष्यों को गौरव ौर अधिकारों के मामले में जन्मजात स्वतन्त्रता ौर समानता प्राप्त हैं", True)
+#convert_to_hindi("⠁⠝⠥⠉⠈⠡⠑⠙⠀⠼⠁⠀⠎⠘⠔⠀⠍⠝⠥⠯⠈⠽⠕⠰⠀⠅⠕⠀⠛⠪⠗⠧⠀⠪⠗⠀⠁⠮⠊⠅⠜⠗⠕⠰⠀⠅⠑⠀⠍⠜⠍⠇⠑⠀⠍⠑⠰⠀⠚⠝⠈⠍⠚⠜⠞⠀⠎⠈⠧⠞⠝⠈⠞⠈⠗⠞⠜⠀⠪⠗⠀⠎⠍⠜⠝⠞⠜⠀⠏⠈⠗⠜⠏⠈⠞⠀⠓⠌⠰", True)
