f83c2e4454cb863bf6d73549263e25a7f9bbab6b
velansalis <velanmax8@gmail.com>
2019-03-21 21:42:14 +0530

    Deleted redundancy

--- hindi_generator.py
+++ hindi_generator.py
@@ -1,8 +1,8 @@
 f = open("hindi_unicode_beta.txt", "w")
 
 for i in range(2304, 2304+127):
-    print(chr(i))
-    #f.write(str(i) + " - ")
+    print(str(i) + " - " + chr(i))
+    f.write(str(i) + " - ")
     f.write(chr(i))
     f.write("\n")
 
