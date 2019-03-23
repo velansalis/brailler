c576a5ecd8182f94e810c5a45864c6cb0cb5591c
velansalis <velanmax8@gmail.com>
2019-03-23 18:51:40 +0530

    Fixed UI

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 let dataMapping;
 let braille = document.querySelector("#brailleText");
-let string = "";
 let indicator = document.querySelector(".copyIndicator");
+let string = ""; // Variable to hold Braille text
 
 fetch("hindi-braille-mapping.json")
   .then(response =>
@@ -14,17 +14,6 @@ fetch("hindi-braille-mapping.json")
     console.log(err);
   });
 
-// Auto Expanding Textarea
-var textarea = document.querySelector("textarea");
-textarea.addEventListener("keydown", autosize);
-function autosize() {
-  var el = this;
-  setTimeout(function() {
-    el.style.cssText = "height:auto; padding:0";
-    el.style.cssText = "height:" + el.scrollHeight + "px";
-  }, 0);
-}
-
 // Hindi to Braille Conversion
 function convertToBraille(letter) {
   letter = letter.value;
@@ -36,10 +25,13 @@ function convertToBraille(letter) {
       string += letter.charAt(i);
       continue;
     }
-    if (i % 29 == 0 && i != 0) string += "<br>";
   }
-  if (letter.length == 0) string = "Braille will display here";
-  braille.innerHTML = string;
+  if (letter.length == 0) {
+    string = "Braille will display here";
+  } else {
+    string.replace(" ", "1");
+  }
+  braille.innerText = string;
 }
 
 function copy() {
@@ -54,7 +46,3 @@ function copy() {
   document.execCommand("copy", false);
   inp.remove();
 }
-
-window.onload = () => {
-  braille.innerHTML = "Braille will display here";
-};
