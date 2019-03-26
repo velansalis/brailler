4b925f04c52b95d6ba75691da4b4d1264a1432a8
velansalis <velanmax8@gmail.com>
2019-03-26 22:24:42 +0530

    Refactored

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,8 @@
 import { convertToBraille } from "./js/convert";
 import { copy, toggleMenu } from "./js/domevents";
-import "./style.css";
+import "./css/style.css";
 
 // Dom Events
-window.convertToBraille = convertToBraille;
-window.copy = copy;
-window.toggleMenu = toggleMenu;
+window.convertToBraille = convertToBraille; // Convert to Braille click
+window.copy = copy; // Copy click
+window.toggleMenu = toggleMenu; // Toggle menu click
