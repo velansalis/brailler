918a07e40436192c238ef0bc36ccf8398dd700e6
velansalis <velanmax8@gmail.com>
2019-03-30 17:22:32 +0530

    Updated gitignore

--- dist/js/loadjson.js
+++ dist/js/loadjson.js
@@ -3,8 +3,8 @@ let dataMapping = {};
 let url =
   "https://gist.githubusercontent.com/velansalis/a467db04321a29414cd87e04b0c901d1/raw/4ad3677f9ac3397b52039cb6f367b923896e1e5f/hindi-to-braille-mapping.json";
 
-window.onload = async () => {
-  await fetch(url)
+window.onload = () => {
+  fetch(url)
     .then(response =>
       response.json().then(data => {
         dataMapping = data;
