d92a3c8b8a20a1b6938134af8ed0ada442e57c33
velansalis <velanmax8@gmail.com>
2019-03-30 16:55:08 +0530

    Deleted JSON

--- dist/js/loadjson.js
+++ dist/js/loadjson.js
@@ -1,14 +1,16 @@
 import { braille, fromLang } from "./domnodes";
-let dataMapping;
+let dataMapping = {};
+let url =
+  "https://gist.githubusercontent.com/velansalis/a467db04321a29414cd87e04b0c901d1/raw/4ad3677f9ac3397b52039cb6f367b923896e1e5f/hindi-to-braille-mapping.json";
 
-window.onload = () => {
-  fetch("data/hindi-braille-mapping.json")
+window.onload = async () => {
+  await fetch(url)
     .then(response =>
       response.json().then(data => {
         dataMapping = data;
         braille.innerText = "";
         fromLang.innerHTML = "";
-        console.log("Hindi JSON loaded..");
+        console.log("JSON loaded");
       })
     )
     .catch(err => {
