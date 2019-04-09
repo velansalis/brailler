b4fea3d4f27a43fdacb2f2388f2a2e93f5bdcce1
velansalis <velanmax8@gmail.com>
2019-04-09 11:00:38 +0530

    Updated locally

--- dist/js/loadjson.js
+++ dist/js/loadjson.js
@@ -1,7 +1,18 @@
 let dataMapping = {};
 let gistid = "a467db04321a29414cd87e04b0c901d1";
+let json_name = "hin_to_brl.json";
 
 window.onload = () => {
+  fetch(json_name)
+    .then(response => response.json())
+    .then(myJson => {
+      console.log("Json loaded");
+      dataMapping = myJson;
+    })
+    .catch(err => console.err(err));
+};
+
+const getJsonFromGit = () => {
   fetch("https://api.github.com/gists/" + gistid)
     .then(response => response.json())
     .then(myJson => {
@@ -9,6 +20,16 @@ window.onload = () => {
       dataMapping = JSON.parse(
         myJson.files[Object.keys(myJson.files)[0]].content
       );
+      return dataMapping;
+    })
+    .catch(err => console.err(err));
+};
+
+const getJsonFromLocal = () => {
+  fetch(json_name)
+    .then(response => response.json())
+    .then(myJson => {
+      return myJson;
     })
     .catch(err => console.err(err));
 };
